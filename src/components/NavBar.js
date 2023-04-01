4e7116101dc1350e4642553c1e616bdc0b3da219
Manuel Garcia Pettinari <manuel.garciapettinari@gmail.com>
2023-04-01 19:27:13 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+function NavBar() {
+    return (
+        <>
+            <nav>
+                <div>
+                    <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Wishlist</li>
+                    <li>Cart</li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default NavBar;
